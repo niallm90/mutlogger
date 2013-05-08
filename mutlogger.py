55fa5f5ca5ecc71e11e71a8851808b179cfa3218
Niall McAndrew <niallm90@gmail.com>
2013-05-08 14:30:22 +1200

    Logging to CSV.

---
+++ mutlogger.py
@@ -0,0 +1,59 @@
+import argparse, csv, datetime, os.path
+from ctypes import *
+
+#Load library.
+cdll.LoadLibrary("libftdimut.so")
+lib = CDLL("libftdimut.so")
+
+def parseData(argsData):
+  data = []
+  for d in argsData:
+    title, request = d.split(":")
+    new = {'title': title, 'request':request}
+    data.append(new)
+  return data
+
+def csvHeaders():
+  out = []
+  for d in data:
+    out.append(d['title'])
+  return out
+
+def csvRow():
+  out = []
+  for d in data:
+    out.append(str(lib.ftdimut_getData(int(d['request'], 0))))
+  return out
+
+parser = argparse.ArgumentParser(description='Log MUT data using an openport 1.3 cable.')
+parser.add_argument("-t", "--testing", action='store_true', help='Enable testing in the libftdimut library.')
+parser.add_argument("-o", "--output", help='Output file.')
+parser.add_argument("data", metavar='Name:Request', nargs='+', help='Data values to grab.')
+
+args = parser.parse_args()
+data = parseData(args.data)
+
+if(args.testing):
+  lib.ftdimut_setTesting(True)
+
+if args.output is None:
+  args.output = datetime.datetime.now().isoformat().replace("T", "_") + ".csv"
+
+if(os.path.exists(args.output)):
+  print("Output file exists!")
+  exit(2)
+
+lib.ftdimut_setup()
+lib.ftdimut_init()
+
+import csv
+with open(args.output, 'w', newline='') as csvfile:
+  spamwriter = csv.writer(csvfile, delimiter=' ',
+                          quotechar='|', quoting=csv.QUOTE_MINIMAL)
+  spamwriter.writerow(csvHeaders())
+  try:
+    while(True):
+      spamwriter.writerow(csvRow())
+  except KeyboardInterrupt:
+    exit(0)
+
